43824e68536dafdff5f041be9e140f9c7d57c6e6
Amelia Lin <amelialin@amelias-air.hbs.edu>
2014-11-17 17:41:04 -0500

    made palindrome into a function

--- Problems/palindrome.py
+++ Problems/palindrome.py
@@ -2,8 +2,6 @@ from sys import argv
 
 script, word = argv
 
-length = len(word) - 1
-
 # ==PSEUDOCODE==
 # if the word has an even num of chars
 	# pair up the chars until word[i] where i = length/2
@@ -12,16 +10,18 @@ length = len(word) - 1
 
 # However, in practice bc of the way math is calculated in Python, you should just be able to take length/2 for both cases; e.g. 5/2 = 2
 
-i = 0
+def palindrome(word):
+	length = len(word) - 1
+	i = 0
+	while i <= length/2:
+		# print i
+		if word[i] == word[length-i]:
+			# print word[i], "matches", word[length-i]
+			pass
+		else:
+			# print word[i], "does not match", word[length-i]
+			return False
+		i += 1 # increment position
+	return True
 
-while i <= length/2:
-	print i
-	if word[i] == word[length-i]:
-		print word[i], "matches", word[length-i]
-	else:
-		print word[i], "does not match", word[length-i]
-		print "Not a palindrome."
-		exit()
-	i += 1 # increment position
-
-print "It's a palindrome."
+print palindrome(word)
