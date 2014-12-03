91b4bf7bcd5ceb9dcb993af88299e64995365dcf
Amelia Lin <amelia.lin0@gmail.com>
2014-12-03 01:17:47 -0500

    Added commented versions of permutations.py and Keir added his version

--- Problems/permutations.py
+++ Problems/permutations.py
@@ -3,41 +3,25 @@
 def permutations(string):
     length_of_string = len(string)
     all_permutations = []
-    current_permutation_list = "" # initialize empty string
-    remainder_string = string # for first iteration, the 
-        # remainder is the entire string
-    remainder_list = list(remainder_string) # transform 
-        # into list form
-    print "Input string: %s Length of string: %d" % (string, length_of_string)
+    current_permutation_list = ""
+    remainder_string = string
+    remainder_list = list(remainder_string)
     permutations_helper(current_permutation_list, remainder_string, all_permutations)
     return all_permutations
 
-def permutations_helper(current_permutation_list, remainder_string, all_permutations): # takes more 
-        # inputs to pass values of variables between
-        # levels of recursion
+def permutations_helper(current_permutation_list, remainder_string, all_permutations):
     remainder_list = list(remainder_string) 
     length_of_remainder = len(remainder_list)
-    print "current_permutation_list:", current_permutation_list, "remainder_list:", remainder_list, "with", len(remainder_list), "elements left. all_permutations:", all_permutations
     if len(remainder_list) == 1:
-        print "Only 1 element left"
-        print "current_permutation_list:", current_permutation_list
         current_permutation_list += remainder_list[0]
-        print "current_permutation_list:", current_permutation_list
         all_permutations.append(current_permutation_list)
-        print "all_permutations:", all_permutations
-        print "FINAL All Permutations:", all_permutations
-        # return all_permutations
     else:
-        print "%d elements left" % length_of_remainder
-        print range(length_of_remainder)
         for i in range(length_of_remainder):
-            print "i:", i
             current_permutation_sublist = current_permutation_list[:]
             current_permutation_sublist += remainder_list[i]
             remainder_sublist = remainder_list[:]
             del remainder_sublist[i]
             remainder_string = ''.join(remainder_sublist)
-            print "Running recursion. current_permutation_sublist:", current_permutation_sublist, "remainder_sublist:", remainder_sublist, "all_permutations:", all_permutations
             permutations_helper(current_permutation_sublist, remainder_string, all_permutations)
 
 if __name__ == "__main__":
@@ -48,7 +32,6 @@ if __name__ == "__main__":
 # ==PSEUDOCODE==
 
 # ==Recursive Soln==
-# create tracker list that records what branch of the total possibility tree you are at
 # If the REMAINDER set has > 1 character left
     # take the next character and append it to the end of the PERMUTATION string
     # mark that this branch has been taken
@@ -59,9 +42,30 @@ if __name__ == "__main__":
 # for (0, number_of_characters)
 # append each result to an array, total array will contain all permutations. There will always be number_of_chracters! (factorial) number of results
 
-# 123
-# 132
-# 213
-# 231
-# 312
-# 321
+def comments_permutations(string):
+    length_of_string = len(string)
+    all_permutations = [] # initialize empty result list
+    current_permutation_list = "" # initialize empty string for current permutation head
+    remainder_string = string # for first iteration, the remainder is the entire string
+    remainder_list = list(remainder_string) # transform string into list form
+    print "current_permutation_list:", current_permutation_list, "remainder_list:", remainder_list
+    comments_permutations_helper(current_permutation_list, remainder_string, all_permutations) # run permutations_helper, which will build all_permutations for us
+    return all_permutations # simply return the updated all_permutations list
+
+def comments_permutations_helper(current_permutation_list, remainder_string, all_permutations): # this helper function takes more inputs to pass information between levels of recursion. all_permutations is simply a *reference* to the list, which is accessible at all levels of the recursion.
+    remainder_list = list(remainder_string) # listify the string you passed in
+    length_of_remainder = len(remainder_list)
+    if len(remainder_list) == 1: # if there's only one character left (only 1 possibility what's coming next):
+        print "current_permutation_list:", current_permutation_list, "remainder_list:", remainder_list
+        current_permutation_list += remainder_list[0] # append the lone character to the end of the current_permutation_list to complete the permutation
+        all_permutations.append(current_permutation_list) # append the completed permutation to the master list all_permutations
+        print "all_permutations:", all_permutations
+    else: # if there is >1 character left:
+        for i in range(length_of_remainder): # for each possible *next* character (since there is >1 possibility what the next char is):
+            current_permutation_sublist = current_permutation_list[:] # make a copy of the current "head" of the word. A *separate* copy of current_permutation_sublist will live in *each* level of recursion!
+            current_permutation_sublist += remainder_list[i] # append the character to the head
+            remainder_sublist = remainder_list[:] # make a copy of the current "remainder" of the word
+            del remainder_sublist[i] # delete the character from the remainder, since it has now been "used up"
+            print "current_permutation_sublist:", current_permutation_sublist, "remainder_sublist:", remainder_sublist            
+            remainder_string = ''.join(remainder_sublist) # stringify the remaining remainder, to be used as input in next level of recursion
+            comments_permutations_helper(current_permutation_sublist, remainder_string, all_permutations) # find all permutations of the new, shorter remainder
--- Problems/permutations_keir.py
+++ Problems/permutations_keir.py
@@ -16,3 +16,19 @@ if __name__ == "__main__":
   from sys import argv
   script, string = argv
   print permutations(string)
+
+# my version uses default arguments
+# to detect the "starting" case
+# to avoid a helper function
+# 11:15
+# Me
+# i don't know what that means
+# 11:15
+# Keir Mierle
+# if you want look it up
+# python default arguments
+# basically
+# my function can get invoked either as
+# permutation(x)
+# permutation(x, y)
+# permutation(x, y, z)
