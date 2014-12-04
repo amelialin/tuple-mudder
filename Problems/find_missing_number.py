bf4cb1d4ec6d53277354be2516634f61a4fabcef
Amelia Lin <amelia.lin0@gmail.com>
2014-12-04 17:52:22 -0500

    Corrected unit tests, wrote docstrings, now takes array as input

--- Problems/find_missing_number.py
+++ Problems/find_missing_number.py
@@ -1,24 +1,24 @@
 # find_missing_number - Given an array of size N with integers 1 to N+1, but missing one, find the missing number.
 
+from sys import argv
+import random
+from random import shuffle
+
 def generate_random_array():
-    # random.seed(10) # should I let the seed number be an input? How do I write unit tests for this problem?
-    N = random.randint(1, 40)
+    N = random.randint(1, 10000)
     array = [i for i in range(1, N + 1)]
-    # random.seed(10)
     missing_number = random.randint(1, N)
     array.remove(missing_number)
     shuffle(array)
     print array
-    return array, N 
+    return array, missing_number 
 
-def find_missing_number():
-    array, N = generate_random_array()
+def find_missing_number(array):
+    """Given an array of size N with integers 1 to N+1, but missing one, finds the missing number."""
+    N = len(array) + 1
     return N * (N + 1) / 2 - sum(array)
 
 if __name__ == "__main__":
-    from sys import argv
-    import random
-    from random import shuffle
     print find_missing_number()
 
 # ==PSEUDOCODE==
@@ -31,9 +31,8 @@ if __name__ == "__main__":
         # return that number
 
 def comments_generate_random_array():
-    # random.seed(10)
+    # random.seed(10) # can use to set seed
     N = random.randint(1, 40) # randomly generate N
-    # print N
     array = [i for i in range(1, N + 1)] # define array of values from 1 to N
     # random.seed(10)
     missing_number = random.randint(1, N) # randomly select number to delete from array
@@ -41,27 +40,28 @@ def comments_generate_random_array():
     array.remove(missing_number) # delete number from array
     shuffle(array)
     print array
-    return array, N # return finished array
+    return array, missing_number # return finished array
 
-def comments_find_missing_number():
-    array, N = comments_generate_random_array() # get random array and N
-    # print array
-    # print N
+def comments_find_missing_number(array):
+    N = len(array) + 1 # rederive N, for accuracy
+    return (N) * (N + 1) / 2 - sum(array) # find theoretical sum of array elements minus actual sum of array elements
+    # less efficent: iterate through entries
     # for i in range(1, N + 1): # go through array from 1-N and search array for that value
     #     try:
     #         array.index(i) # look for that value in the array
     #     except ValueError: # if not found:
     #         return i # return the missing number
-    return (N) * (N + 1) / 2 - sum(array) # find theoretical sum of array elements minus actual sum of array elements
+
+
 
 # Extra work on this problem from Keir
 #
-#   1. Make the function take a single array argument (passing N is
+#   1. DONE Make the function take a single array argument (passing N is
 #      unnecessary, you can derive N from the length of the array)
-#   2. Give the function a docstring.
-#   3. Write unit tests for the function.
-#      a. Write tests with explicit inputs (e.g. [1, 2, 4])
-#      b. Write tests with generated input (like your generate random array)
+#   2. DONE Give the function a docstring.
+#   3. DONE Write unit tests for the function.
+#      a. DONE Write tests with explicit inputs (e.g. [1, 2, 4])
+#      b. DONE Write tests with generated input (like your generate random array)
 #   4. Extend the script using argparse (longer, means learning argparse)
 #      a. To take a random seed
 #      a. To take N
--- Problems/find_missing_number_test.py
+++ Problems/find_missing_number_test.py
@@ -1,5 +1,12 @@
-from find_missing_number_test import find_missing_number_test
+from find_missing_number import *
 
-def test_find_missing_number():
-    assert
-    assert not
+def test_find_missing_number_explicit():
+    """Tests with explicit input."""
+    assert find_missing_number([1, 2, 4]) == 3
+    assert find_missing_number([23, 9, 7, 6, 22, 2, 20, 15, 19, 1, 11, 18, 4, 21, 8, 3, 12, 16, 17, 5, 13, 10]) == 14
+
+def test_find_missing_number_random():
+    """Tests with generated input."""
+    for i in range(1000):
+        array, missing_number = generate_random_array()
+        assert find_missing_number(array) == missing_number
