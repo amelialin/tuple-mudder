558ead6250235d0726c83f932f2f5dd59b15ed03
Amelia Lin <amelia.lin0@gmail.com>
2015-02-02 17:03:17 -0500

    Cleaned up anonymous_letter and anonymous_letter_files, added computational complexity

--- Problems/anonymous_letter_files.py
+++ Problems/anonymous_letter_files.py
@@ -1,16 +1,28 @@
-# Write a function that takes two documents: an anonymous letter L and the text from a magazine M. Your function should return true if L can be written with words from M. Words from M can only be used in L as many times as they occur in M, just as though you were physically cutting up a magazine and pasting the words together.
+# Write a function that takes two documents: an anonymous letter L and the text 
+# from a magazine M. Your function should return true if L can be written with 
+# words from M. Words from M can only be used in L as many times as they occur 
+# in M, just as though you were physically cutting up a magazine and pasting 
+# the words together.
 
 from frequent_words import make_all_lowercase, strip_punctuation
 
 def anonymous_letter_files(L_file, M_file):
-    """Takes 2 filenames as input, which contain text. Returns True if the text in L can be made from words in M, False if it cannot. Case insensitive and punctuation insensitive."""
-    L_words = find_word_frequencies(make_all_lowercase(strip_punctuation(open(L_file).read())))
-    M_words = find_word_frequencies(make_all_lowercase(strip_punctuation(open(M_file).read())))
-    if set(L_words.keys()).issubset(set(M_words.keys())): # test if all words in L are in M
+    """Takes 2 filenames as input, which contain text. Returns True if the text 
+    in L can be made from words in M, False if it cannot. Case insensitive and 
+    punctuation insensitive."""
+    L_words = find_word_frequencies(
+        make_all_lowercase(strip_punctuation(open(L_file).read())))
+    M_words = find_word_frequencies(
+        make_all_lowercase(strip_punctuation(open(M_file).read())))
+    
+    # test if all words in L are in M
+    if set(L_words.keys()).issubset(set(M_words.keys())): 
         pass
     else:
         return False
-    for word in L_words.keys(): # test if all frequencies of words in L are <= frequencies of words in M
+    
+    # test if all frequencies of words in L are <= frequencies of words in M
+    for word in L_words.keys(): 
         print word, L_words[word], M_words[word]
         if L_words[word] <= M_words[word]:
             pass
@@ -19,11 +31,11 @@ def anonymous_letter_files(L_file, M_file):
     return True
 
 def find_word_frequencies(string): # modified frequent_words
-    """Takes input as a string, returns dict of all words, made lowercase, and their frequencies. Case insensitive. Input will be stripped 
+    """Takes input as a string, returns dict of all words, made lowercase, 
+    and their frequencies. Case insensitive. Input will be stripped 
     of common punctuation."""
     word_frequencies = {}
-    string = make_all_lowercase(strip_punctuation(string)) 
-        # clean up string
+    string = make_all_lowercase(strip_punctuation(string)) # clean up string
     words = string.split()
     for word in words:
         if word_frequencies.get(word) == None: 
@@ -36,9 +48,3 @@ if __name__ == "__main__":
     from sys import argv
     script, L_file, M_file = argv
     print anonymous_letter_files(L_file, M_file)
-
-# ==PSEUDOCODE==
-
-# You could do something really hacky where you go through all the combinatorial possibilities for every subset of M and see if L equals any of them
-
-# Better would be to at least get the total set of all words from L and set of all words from M, and make sure that L is in M. Then, check the *counts* for each of those words and make sure that the count for each word in L is <= the count of the same word in M. Seems like a dict would be good here, similar to frequent_words. Honestly using frequent_words and just plugging L and M into it, then comparing, seems like a perfectly reasonable way to go about this.
--- Problems/anonymous_letter_files_test.py
+++ Problems/anonymous_letter_files_test.py
@@ -3,5 +3,5 @@ from anonymous_letter_files import anonymous_letter_files
 # why didn't I have to import all the helper functions too?
 
 def test_anonymous_letter_files():
-    assert anonymous_letter_files("anonymous_letter_test1.txt", "anonymous_letter_test2.txt") == True
-    assert anonymous_letter_files("anonymous_letter_test3.txt", "anonymous_letter_test4.txt") == False
+    assert anonymous_letter_files("anonymous_letter_files_test1.txt", "anonymous_letter_files_test2.txt") == True
+    assert anonymous_letter_files("anonymous_letter_files_test3.txt", "anonymous_letter_files_test4.txt") == False
