7bd20ae294b0c3649d8e2e463fe9a6f9cd3c3b8e
Amelia Lin <amelia.lin0@gmail.com>
2015-02-22 19:55:44 -0500

    Ex 44

---
+++ ex44a.py
@@ -0,0 +1,15 @@
+# inheritance - implicit inheritance
+
+class Parent(object):
+
+    def implicit(self):
+        print "PARENT implicit()"
+
+class Child(Parent):
+    pass
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
